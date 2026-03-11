421679ea817a38feb91c1f7bc1db04e5cf65be0e
Edmundo574 <265771939+ramirezzedmundo-lgtm@users.noreply.github.com>
2026-03-10 20:54:55 -0600

    Create carrusel.js

---
+++ js/carrusel.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll('.slide');
+let current = 0;
+
+document.getElementById('next').addEventListener('click', () => {
+  slides[current].classList.remove('active');
+  current = (current + 1) % slides.length;
+  slides[current].classList.add('active');
+});
+
+document.getElementById('prev').addEventListener('click', () => {
+  slides[current].classList.remove('active');
+  current = (current - 1 + slides.length) % slides.length;
+  slides[current].classList.add('active');
+});
